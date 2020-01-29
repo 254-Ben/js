4fbf1e3820f363eef809232380ac7ec53289507d
Carol254 <paulomugoo@gmail.com>
2020-01-29 16:43:45 +0300

    create new person object

---
+++ data_types.js
@@ -0,0 +1,10 @@
+// understanding objects in .js
+let person = {
+  name: "Ronnie Kabala",
+  email: "kabalronnie@gmail.com",
+  gender: "Male",
+  occuppation: "Student Software Engineer"
+};
+// Reference a value from key value pair in person object
+let jina = person.name;
+console.log("my name is " + jina + " and I am a " + occupation);
